af70f319dda9ff32e7eec4bd44e13bd32c033210
Henrik Hannewijk <henrik@designated.nl>
2024-06-05 08:07:13 +0200

    fix for no script in head

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,7 +1,6 @@
 import { GoogleTagManager } from '@next/third-parties/google';
 import moment from 'moment';
 import type { AppProps } from 'next/app';
-import type Script from 'next/script';
 import { useRouter } from 'next/router';
 import { strings } from '../lib/constants/languages/strings';
 import '../styles/globals.scss';
@@ -16,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Component {...pageProps} />
 
     {/* Cookiebot recurrance fix */}
-    <Script strategy="beforeInteractive" id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="7431cf9b-4fcd-418a-b2a5-49f8e888a261" data-blockingmode="auto"></Script>
+    <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="7431cf9b-4fcd-418a-b2a5-49f8e888a261" data-blockingmode="auto" type="text/javascript" async></script>
 
     <GoogleTagManager gtmId="GTM-MCTQDSQM" />
   </>
